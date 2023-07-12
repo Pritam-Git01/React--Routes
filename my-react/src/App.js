40bf9108b2f762dfe288e54e36f0fc94fd97ca0d
Pritam-Git01 <pritamyadav.ds.work.@gmail.com>
2023-07-12 19:10:13 +0530

    except edit everything working

--- my-react/src/App.js
+++ my-react/src/App.js
@@ -1,14 +1,12 @@
-
-import './App.css';
-
+import "./App.css";
+import Form from "./components/Form";
 
 function App() {
   return (
-   <div className='App'>
-      
-      </div>
-    )
- 
+    <div className="App">
+      <Form />
+    </div>
+  );
 }
 
 export default App;
---
+++ my-react/src/components/Form.js
@@ -0,0 +1,117 @@
+import React, { useState } from "react";
+import styles from "./Form.module.css";
+import TextField from "@mui/material/TextField";
+import { Button } from "@mui/material";
+import { v4 as uuid } from "uuid";
+
+const Form = () => {
+  const [data, setData] = useState([]);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const [edit,setEdit] = useState(false);
+
+  const [show, setshow] = useState(false);
+
+  function handleSubmit() {
+    let studentData = {
+      id: uuid(),
+      name: name,
+      email: email,
+    };
+
+
+        setData([...data, studentData]);
+        setshow(true);
+        setEmail("");
+        setName("");
+    
+    }
+    
+    
+  
+ 
+
+  const handleDelete = (id) => {
+    let newData = data.filter((i) => i.id === id);
+    setData(newData)
+    
+  }
+
+  const handleEdit= (id) => {
+    let newData = data.find((i) => i.id === id);
+    setName(newData.name)
+    setEmail(newData.email)
+    setEdit(true)
+ 
+
+  }
+
+  return (
+    <div className={styles.wraper}>
+      <div className={styles.container}>
+        <h1>Student Form</h1>
+
+        <div className={styles.inputs}>
+          <TextField
+            sx={{ width: "15vw", marginTop: "1rem" }}
+            onChange={(e) => setName(e.target.value)}
+            label="Name"
+            variant="standard"
+            type="text"
+            value={name}
+          />
+          <TextField
+            sx={{ width: "15vw", marginTop: "1rem" }}
+            onChange={(e) => setEmail(e.target.value)}
+            label="E-mail"
+            variant="standard"
+            type="text"
+            value={email}
+          />
+
+          <Button
+            onClick={handleSubmit}
+            sx={{ marginTop: "2rem" }}
+            variant="contained"
+          >
+            {edit ? "Update":'Submit'}
+          </Button>
+        </div>
+
+        {show ? (
+          <div className={styles.result}>
+            <table>
+              <thead>
+                <tr>
+                  <td>Name</td>
+                  <td>E-mail</td>
+
+                  <td>Action</td>
+                </tr>
+              </thead>
+              <tbody>
+                {data.map((item) => {
+                  return (
+                    <tr key={item.id}>
+                      <td>{item.name}</td>
+                      <td>{item.email}</td>
+                      <td>
+                        <Button variant="contained" onClick={() => handleEdit(item.id)}>Edit</Button>
+                        <Button variant="contained" onClick={() => handleDelete(item.id)}>Delete</Button>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Form;
