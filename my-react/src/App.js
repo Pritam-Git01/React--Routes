004a156d378cd98887736770dfcf50db95b4f544
Pritam Kumar Yadav <pritamyadav.ds.work@gmail.com>
2023-10-31 21:22:45 +0530

    device detect successfully

--- my-react/src/App.js
+++ my-react/src/App.js
@@ -1,11 +1,10 @@
 
-import './App.css';
-
+import DeviceTest from './components/DeviceTest';
 
 function App() {
   return (
    <div className='App'>
-      
+     <DeviceTest/>
       </div>
     )
  
